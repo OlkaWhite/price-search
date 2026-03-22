98f520c789b93bc5ef61d49574b67d98f8b2f131
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-23 01:12:17 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,24 @@
 import "./globals.css";
+import Header from "../components/Header";
+
 export const metadata = {
-  title: "Price Search",
+  title: "b2bpart.ru",
   description: "Поиск по прайсам"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body style={{ fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Arial" }}>
-        {children}
+      <body
+        style={{
+          margin: 0,
+          fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Arial",
+          background: "#fafafa",
+          color: "#111"
+        }}
+      >
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
