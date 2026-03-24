bb120ab9555ee6d674d8839ccdfca13a374d761f
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:57:37 +0300

    Create page.js

---
+++ app/admin/analytics/page.js
@@ -0,0 +1,8 @@
+export default function AdminAnalyticsPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Аналитика</h1>
+      <p>Здесь будет аналитика поисковых запросов и поведения пользователей.</p>
+    </div>
+  );
+}
