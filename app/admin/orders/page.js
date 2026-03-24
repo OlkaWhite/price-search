b5e72f6e572fc66c0e23d84ea1fa0121f38e8280
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:56:17 +0300

    Create page.js

---
+++ app/admin/orders/page.js
@@ -0,0 +1,8 @@
+export default function AdminOrdersPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Заказы</h1>
+      <p>Здесь будет список заказов, карточка заказа и статусы.</p>
+    </div>
+  );
+}
