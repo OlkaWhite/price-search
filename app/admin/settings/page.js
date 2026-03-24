72be178863ee8bf6cf12de287239190a2a7d43a2
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:58:55 +0300

    Create page.js

---
+++ app/admin/settings/page.js
@@ -0,0 +1,8 @@
+export default function AdminSettingsPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Настройки</h1>
+      <p>Здесь будут глобальные курсы валют, webhook и системные параметры.</p>
+    </div>
+  );
+}
