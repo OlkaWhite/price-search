a7932d898d3edee881778371bc3874d9f57a6515
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:58:13 +0300

    Create page.js

---
+++ app/admin/brands/page.js
@@ -0,0 +1,8 @@
+export default function AdminBrandsPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Бренды</h1>
+      <p>Здесь будет управление brand aliases и canonical брендами.</p>
+    </div>
+  );
+}
