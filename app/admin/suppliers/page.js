6e35898084ed9b7ef1e8f4d8f06b25b4791b44b2
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:54:34 +0300

    Create page.js

---
+++ app/admin/suppliers/page.js
@@ -0,0 +1,8 @@
+export default function AdminSuppliersPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Поставщики</h1>
+      <p>Здесь будет управление поставщиками, курсами и параметрами прайсов.</p>
+    </div>
+  );
+}
