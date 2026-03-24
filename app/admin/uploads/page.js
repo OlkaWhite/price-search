bb62e0906c7bb7bb0dcbeee0d1e78b449aaaebfe
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:55:18 +0300

    Create page.js

---
+++ app/admin/uploads/page.js
@@ -0,0 +1,8 @@
+export default function AdminUploadsPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Загрузка прайсов</h1>
+      <p>Здесь будет загрузка прайсов и история импортов.</p>
+    </div>
+  );
+}
