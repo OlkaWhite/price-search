006724ee0839c9e1c0519dd041c24cc1d77845ab
OlkaWhite <70839944+OlkaWhite@users.noreply.github.com>
2026-03-24 11:56:55 +0300

    Create page.js

---
+++ app/admin/users/page.js
@@ -0,0 +1,8 @@
+export default function AdminUsersPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 0 }}>Клиенты</h1>
+      <p>Здесь будет список пользователей и их активность.</p>
+    </div>
+  );
+}
